6fdc73618662ffe88a52b78004d9f3d3e7a2a2e3
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-02 17:46:30 -0300

    carousel

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,3 +10,22 @@ setInterval(() =>  typeWriter(titulo), 10000);
 const titulo = document.getElementById("text");
 typeWriter(titulo);
 
+// carrosel
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrosel() {
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+setInterval(carrosel, 2500);
+
+//

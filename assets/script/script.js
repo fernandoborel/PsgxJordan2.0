4ec949661728a89f9af864276df94b4d31b3a961
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-03 22:23:44 -0300

    inicio do carrossel

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,20 +12,57 @@ typeWriter(titulo);
 
 // carrosel
 
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
-
-let idx = 0;
-
-function carrosel() {
-    idx++;
-
-    if(idx > img.length - 1){
-        idx = 0;
+class SlideStories {
+    constructor(id){
+        this.slide = document.querySelector(`[data-slide="${id}"]`);
+        this.active = 0;
+        this.init();
+    }
+    activeSlide(index) {
+        this.active = index;
+        this.items.forEach(item => item.classList.remove('active'));
+        this.items[index].classList.add('active');
     }
 
-    imgs.style.transform = `translateX(${-idx * 500}px)`;
+    prev() {
+        if(this.active > 0) {
+            this.activeSlide(this.active - 1);
+        } else {
+            this.activeSlide(this.items.length - 1);
+        }
+    }
+
+    next() {
+        if(this.active < this.items.length -1) {
+            this.activeSlide(this.active + 1);
+        } else {
+            this.activeSlide(0);
+        }
+    }
+
+    addNavigation() {
+        const nextBtn = this.slide.querySelector('.slide-next');
+        const prevBtn = this.slide.querySelector('.slide-prev');
+        nextBtn.addEventListener('click', this.next);
+        prevBtn.addEventListener('click', this.prev);
+    }
+
+    addThumbItems() {
+        this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
+        this.thumbItems = Array.from(this.thumb.children);
+    }
+    
+    init() {
+        this.next = this.next.bind(this);
+        this.prev = this.prev.bind(this);
+        this.items = this.slide.querySelectorAll('.slide-items > *');
+        this.thumb = this.slide.querySelector('.slide-thumb');
+        this.addThumbItems();
+        this.activeSlide(0);
+        this.addNavigation();
+    }
 }
-setInterval(carrosel, 2500);
+
+new SlideStories('slide');
 
 //

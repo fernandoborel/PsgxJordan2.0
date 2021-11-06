f3a0dc06394f715bf2a7988e79f6bbb92d36d23b
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-06 16:25:37 -0300

    verificação quase concluída

--- assets/script/script.js
+++ assets/script/script.js
@@ -76,6 +76,51 @@ class SlideStories {
 
 
 // validador de formulários
+let formValidator = {
+  handleSubmit:(event)=>{
+    event.preventDefault(); //previne que envie
+    let send = true;
+
+    let inputs = form.querySelectorAll('input');
+
+    for(let i=0; i<inputs.length; i++){
+      let input = inputs[i];
+      let check = formValidator.checkInput(input);
+      if(check !== true) {
+        send = false;
+        console.log(check);
+      }
+    }
+    
+    if(send) {
+      form.submit();
+    }
+  }, 
+  checkInput:(input) => {
+    let rules = input.getAttribute('data-rules');
+
+    if(rules !== null) {
+      rules = rules.split('|');
+      for(let k in rules) {
+        let rDetails = rules[k].split('=');
+        switch(rDetails[0]) {
+          case 'required':
+            if(input.value == '') {
+              return 'Este campo é obrigatório!'
+            }
+          break;
+          case 'min':
+
+          break;
+        }
+      }
+    }
+
+    return true;
+  }
+};
 
 
+let form = document.querySelector('.validator');
+form.addEventListener('submit', formValidator.handleSubmit);
 

8ae583ea62752348b7f9a107fe9afa35ad96834c
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-07 12:35:00 -0300

    scroll suave adicionado

--- assets/script/script.js
+++ assets/script/script.js
@@ -161,3 +161,22 @@ let form = document.querySelector('.validator');
 form.addEventListener('submit', formValidator.handleSubmit);
 
 
+// scroll suave
+function subirtela() {
+  window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+  });
+}
+function decidirbotaoscroll() {
+  if(window.scrollY === 0) {
+      // ocultar o botão
+      document.querySelector('.scrollbutton').style.display = 'none';
+  } else {
+      // mostrar o botão
+      document.querySelector('.scrollbutton').style.display = 'block';
+  }
+}
+
+setInterval(decidirbotaoscroll, 1000);
